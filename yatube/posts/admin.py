48d1aacc0dab27698f2bc79760d8bc24118b8796
vshkinder <shkinder123@gmail.com>
2022-01-11 22:49:35 +0300

    upload db

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+# Из модуля models импортируем модель Post
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('text', 'pub_date', 'author')
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+
+
+# класс PostAdmin
+admin.site.register(Post, PostAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title': title
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
